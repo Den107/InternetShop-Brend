12cb3bbd5655f0f44313c0614c785df877caacba
Den107 <deniskrv@gmail.com>
2020-11-26 19:06:06 +0300

    доделал четвертый урок

--- script/find.js
+++ script/find.js
@@ -5,16 +5,23 @@ let findButton = document.getElementById('searchBtn');
 let nameOfProduct = document.querySelectorAll('.fetured-item__title');
 
 findButton.addEventListener('click', findItem);
-
+input.addEventListener('change', findItem);
 
 
 
 function findItem() {
+
     let value = input.value;
+    let re = /[A-Za-z\-\ ]{3,30}/i;
     nameOfProduct.forEach(name => {
-        if (name.innerText !== value) {
-            name.parentNode.style = 'display: none;';
-        }
+        if (name.innerText.toLowerCase().indexOf(value.toLowerCase()) != -1) {
+            name.parentNode.style = 'display: block;';
+        } else if (value === '') {
+            name.parentNode.style = 'display: block;';
+        } else name.parentNode.style = 'display: none;';
     })
 }
+
+
+
 
