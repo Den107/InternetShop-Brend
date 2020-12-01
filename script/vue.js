2cdc9c6cbf7cf4f2dc7ef916627f082929aab8b3
Den107 <deniskrv@gmail.com>
2020-12-01 11:02:11 +0300

    сделал пятый урок

--- script/vue.js
+++ script/vue.js
@@ -1,23 +1,39 @@
 'use strict';
+const myAPI = 'https://raw.githubusercontent.com/Den107/InternetShop-Brend/lesson5/responses';
 
 const app = new Vue({
     el: '#app',
     data: {
         searchLine: '',
         isVisibleCart: false,
+        filtered: [],
+        products: [],
+        catalogUrl: '/getProducts.json',
+        imgCatalog: 'https://placehold.it/100x115'
     },
     methods: {
-        // filterGoods() {
-        //     console.log(this.searchLine);
-        //     const regexp = new RegExp(this.searchLine, 'ig');
-        //     nameOfProduct.forEach(name => {
-        //         if (regexp.test(name.innerText)) {
-        //             name.parentNode.style = 'display: block;';
-        //         } else if (value === '') {
-        //             name.parentNode.style = 'display: block;';
-        //         } else name.parentNode.style = 'display: none;';
-        //     })
-        // },
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
 
+        filterGoods() {
+            let regexp = new RegExp(this.searchLine, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.name));
+        },
+
+
+    },
+    mounted() {
+        this.getJson(`${myAPI + this.catalogUrl}`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                    this.filtered.push(el);
+                }
+            });
     }
 })
